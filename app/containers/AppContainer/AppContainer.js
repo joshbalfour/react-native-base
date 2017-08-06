3548886176042e84796b921fa32db627a91b01e3
Josh Balfour <josh@joshbalfour.co.uk>
2017-08-06 12:37:24 +0100

    restructure

---
+++ app/containers/AppContainer/AppContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native'
+
+import HelloWorld from '../../components/HelloWorld'
+
+class AppContainer extends Component {
+	render() {
+		return (
+			<HelloWorld />
+		)
+	}
+}
+
+export default AppContainer
